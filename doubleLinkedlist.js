5f78c415739732888acdbea85b4940a27a56af58
Ankit Thakur <ankitthakur@192.168.1.2>
2023-07-07 09:45:32 +0530

    if array is sorted and removing duplicates from array done

---
+++ doubleLinkedlist.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(data) {
+        data = null;
+        next = null;
+        previous = null;
+    }
+}
+class DoubleLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+}
