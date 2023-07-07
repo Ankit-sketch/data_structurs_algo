a1709bcc03c60e3641547e9563b2692fb53bdd8e
Ankit_ATR888 <ankit.thakur@antiersolutions.com>
2023-07-07 17:10:02 +0530

    new changes

--- doubleLinkedlist.js
+++ doubleLinkedlist.js
@@ -1,8 +1,8 @@
 class Node {
     constructor(data) {
-        data = null;
-        next = null;
-        previous = null;
+        this.data = data;
+        this.next = null;
+        this.previous = null;
     }
 }
 class DoubleLinkedList {
@@ -11,4 +11,25 @@ class DoubleLinkedList {
         this.tail = null;
         this.size = 0;
     }
+    append(data) {
+        let node = new Node(data);
+        if (this.size == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            let pointer = this.tail;
+            pointer.next = node;
+            pointer.previous = this.tail;
+            this.head = pointer;
+            this.tail = node;
+            this.tail.previous = pointer;
+            this.tail.next = null;
+        }
+        this.size++;
+    }
 }
+
+const dll = new DoubleLinkedList();
+dll.append(1);
+dll.append(2);
+console.log(dll);
